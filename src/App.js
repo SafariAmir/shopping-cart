0da29fdc39f7f03a5556a70a48e609ec91284d14
SafariAmir <amirhosseinsafari19@gmail.com>
2023-07-05 17:24:19 +0330

    shopping

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Route , Navigate, Routes } from 'react-router-dom';
+
+//Component
+import Store from './components/Store';
+import ProductDetails from './components/ProductDetails';
+import Navbar from './components/shared/Navbar';
+import ShopCart from './components/ShopCart';
+
+//Context
+import ProductContextProvider from './context/ProductContextProvider';
+import CartContextProvider from './context/CartContextProvider';
+
+
+const App = () => {
+  return (
+    <ProductContextProvider>
+      <CartContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/cart' element={<ShopCart />} />
+          <Route path='/products/:id' element={<ProductDetails />} />
+          <Route path='/products' element={<Store />} />
+          <Route path='/' element={ <Navigate to="/products" /> } />
+        </Routes>
+      </CartContextProvider>
+    </ProductContextProvider>
+  );
+};
+
+export default App;
---
+++ src/components/ShopCart.js
@@ -0,0 +1,52 @@
+import React , { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+//Components
+import Cart from './shared/Cart';
+
+//Context
+import { CartContext } from '../context/CartContextProvider';
+
+//Style
+import styles from "./ShopCart.module.css";
+
+const ShopCart = () => {
+
+    const { state , dispatch } = useContext(CartContext)
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.cartContainer}>
+                {state.selectedItems.map(item => <Cart key={item.id} data={item} /> )}
+            </div>
+
+                {
+                    state.itemsCounter > 0 && <div className={styles.payments}>
+                        <p><span>Total Items : </span>{state.itemsCounter}</p>
+                        <p><span>Total Payment : </span>{state.total}</p>
+                        <div className={styles.buttonContainer}>
+                            <button className={styles.clear} onClick={()=> dispatch({type: "CHECKOUT"})} >Check Out</button>
+                            <button className={styles.checkout} onClick={()=> dispatch({type: "CLEAR"})} >Clear</button>
+                        </div>
+                    </div>
+                }
+
+                {
+                    state.checkout && <div className={styles.complete}>
+                        <h3>Checked Out Successfully</h3>
+                        <Link to="/" >Buy More</Link>
+                    </div>
+                }
+                
+                {
+                    !state.checkout && state.itemsCounter === 0 && <div className={styles.complete}>
+                        <h3>Want to Buy ?</h3>
+                        <Link to="/" >Go Back To Shop</Link>
+                    </div>
+                }
+
+        </div>
+    );
+};
+
+export default ShopCart;
